f4b650c282b6cbf470fd4d1246888e71a91d8ad4
Latisha Joe <latisha_joe@yahoo.com>
2020-09-26 08:14:17 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+ attr_accessor :path
+
+ def initialize(path)
+   @path = path
+ end
+
+ def files
+   @files = Dir.entries(@path)
+   @files.delete_if {|file| file == "." || file == ".."}
+ end
+
+
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song
+  attr_accessor :artist, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
+  # def artist_nam=(name)
+  #   self.artist = Artist
+  #
